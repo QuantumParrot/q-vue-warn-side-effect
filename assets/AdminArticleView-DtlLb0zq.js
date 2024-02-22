b95e4994d98420c1c315f7ea03364ae7d5267a34
QuantumParrot <ataraxia528491@gmail.com>
2024-02-22 16:36:34 +0800

    Updates

---
+++ assets/AdminArticleView-DtlLb0zq.js
@@ -0,0 +1,1 @@
+import{_ as e,b as t,o as c}from"./index-BbUD4Z56.js";const s={},n={class:"fs-2 mb-5 text-center"};function o(r,_){return c(),t("h3",n,"文章")}const f=e(s,[["render",o]]);export{f as default};
---
+++ assets/IndexView-CVzMJUmx.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-BbUD4Z56.js";const o={};function r(t,n,s,c,_,a){return" Hello World !! "}const d=e(o,[["render",r]]);export{d as default};
